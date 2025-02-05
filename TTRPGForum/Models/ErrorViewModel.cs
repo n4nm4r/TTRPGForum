fae76f53c17150771d19de2a032e9384628fb327
n4nm4r <nancy.martin.nb@gmail.com>
2025-02-05 11:23:35 -0400

    Add project files.

---
+++ TTRPGForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TTRPGForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
